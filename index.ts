c8d402ddf6ea9e5051ecc699a635d2bf0f12e2be
ioetbc <ioetbc@gmail.com>
2023-05-17 12:21:18 +0100

    adding prisma

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("Hello from Yarn");
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+console.log("Prisma loaded");
+
+// A `main` function so that you can use async/await
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const user = await prisma.user.create({
+    data: {email: "email@email.com", name: "Test"},
+  });
+
+  const post = await prisma.post.create({
+    data: {
+      title: "My Post",
+      content: "My post content",
+      author: {connect: {id: user.id}},
+    },
+  });
+
+  console.log({post});
+  return post;
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
